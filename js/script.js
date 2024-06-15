0589187745476c39b2d38819e19ccbb1bcfea588
Asterfayazia <70164021+vflawlest@users.noreply.github.com>
2024-06-15 16:46:49 +0700

    Update script.js
    Untuk mengaktifkan fitur kirim pesan lewat wa

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ const navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("aktif");
 };
+// Form kirim ke WA
+document.getElementById("whatsappForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var name		= document.getElementById("name").value;
+  var phone 		= document.getElementById("phone").value;
+  var message 	= document.getElementById("message").value;
+  let contact 	= '+6281573283600'; // isi dengan nomor WA (+6283811223344)
+
+  var encodedMessage = encodeURIComponent(
+      "Nama Depan	: " + name + "\n" +
+      "No Whatsapp		: " + phone + "\n" +
+      "Pesan		: " + message
+  );
+
+  var link;
+
+  // Check if user is on a mobile device
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      link = `whatsapp://send?phone=${contact}&text=${encodedMessage}`;
+  } else { // Desktop device
+      link = `https://api.whatsapp.com/send?phone=${contact}&text=${encodedMessage}`;
+  }
+
+  window.open(link, '_blank');
+});
